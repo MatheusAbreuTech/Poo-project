1ffeb018c14cd8a876902f4bd64d055b580c4166
igorzclick <ii.igoralmeida@gmail.com>
2024-10-25 11:06:56 -0300

    first commit

---
+++ main.py
@@ -0,0 +1,17 @@
+from entidades import Veiculo_service , Cliente_service
+
+# Veiculo_service.cadastro_veiculo('punto','fiat','1990','23000','azul','4','QKJD-2456')
+
+
+
+# Cliente_service.cadastro_cliente('Igor' , '04194240' , '112345678' , '123@gmail')
+
+# Cliente_service.listar_cliente()
+
+# Veiculo_service.uptade_veiculos(1,'punto','chevrolet','1990','23000','azul','4','QKJD-2456')
+
+# Veiculo_service.lista_veiculos()
+
+# Cliente_service.update_cliente(1,'Jonatan' , '04194240' , '112345678' , '123@gmail')
+
+# Cliente_service.delete_cliente(1)
